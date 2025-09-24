cd5a4b0cd281ff49fd744e2ca6f014ac19953ee7
devmcibot <devmcibot@gmail.com>
2025-09-24 18:40:46 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },     // <- NÃO bloquear build por ESLint
-  typescript: { ignoreBuildErrors: true },  // <- (opcional) NÃO bloquear por TS
+  /* config options here */
 };
 
 export default nextConfig;
