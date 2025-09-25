bdb81e2ad07be594457074a99980deb7e2089a90
devmcibot <devmcibot@gmail.com>
2025-09-25 12:42:20 -0300

    Update realtime.ts

--- src/lib/realtime.ts
+++ src/lib/realtime.ts
@@ -1,5 +1,5 @@
 // src/lib/realtime.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL!;
 const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!;
